4b50d8e074451e6e886d1283c924d4f2935eaaa9
and-gurin <and.gurin@gmail.com>
2023-10-19 17:45:57 +0200

    add explore page mobile view

---
+++ src/components/searchPanel/searchPanel.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Search from '../../icons/search';
+import './searchPanel.css'
+import CustomButton from "../../UI/button/button";
+
+const SearchPanel = () => {
+    return (
+        <div className='search-panel'>
+            <Search/>
+            <input className='search-panel__input' placeholder='Search'/>
+            <CustomButton content={
+                <span className='search-panel__button'>
+                    Search
+                </span>
+            }/>
+        </div>
+    );
+};
+
+export default SearchPanel;
