069517bcc2d0ca0098cb73a4702809aa73f89dda
zumbost1k <matusevich.mi@gmail.com>
2023-10-21 17:20:10 +0300

    fix rebase bugs

--- src/components/searchPanel/searchPanel.jsx
+++ src/components/searchPanel/searchPanel.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import Search from '@/icons/search';
-import './searchPanel.css'
-import CustomButton from "@/UI/button/button";
-
+import './searchPanel.css';
+import CustomButton from '@/UI/customButton/cistomButton';
 const SearchPanel = () => {
-    return (
-        <div className='search-panel'>
-            <div className='search-panel__container'>
-                <div className='search-panel__search'>
-                    <Search/>
-                    <input placeholder='Search'/>
-                </div>
-                <CustomButton content={
-                    <span className='search-panel__button'>
-                    Search
-                </span>
-                }/>
-            </div>
+  return (
+    <div className='search-panel'>
+      <div className='search-panel__container'>
+        <div className='search-panel__search'>
+          <Search width={'24'} height={'24'} />
+          <input placeholder='Search' />
         </div>
-    );
+        <CustomButton
+          content={<span className='search-panel__button'>Search</span>}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SearchPanel;
