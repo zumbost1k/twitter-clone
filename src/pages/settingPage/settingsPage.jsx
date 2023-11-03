bf238a08591e5b4c072e35bf68f1db5d6cccbdeb
zumbost1k <matusevich.mi@gmail.com>
2023-11-03 12:51:58 +0300

    add setting block

--- src/pages/settingPage/settingsPage.jsx
+++ src/pages/settingPage/settingsPage.jsx
@@ -2,37 +2,111 @@ import React, { useState } from 'react';
 import './settingsPage.css';
 import { useSelector } from 'react-redux';
 import { selectCurrentUser } from '@/selectors/selectors';
+import SettingBlock from '@/UI/settingBlock/settingBlock';
 
 const SettingsPage = () => {
   const currentUsersProfile = useSelector(selectCurrentUser);
   const [userAvatar, setUserAvatar] = useState(
-    currentUsersProfile.profileAvatar
+    `./photos/usersAvatar/${currentUsersProfile.profileAvatar}`
   );
   const [userBackground, setBackground] = useState(
-    currentUsersProfile.profileBackgroundImagePath
+    `./photos/profileBackgrounds/${currentUsersProfile.profileBackgroundImagePath}`
+  );
+  const [userName, setUserName] = useState(currentUsersProfile.userName);
+  const [userNickName, setUserNickName] = useState(
+    currentUsersProfile.nickName
+  );
+  const [userProfileDescription, setUserProfileDescription] = useState(
+    currentUsersProfile.profileDescription
   );
   return (
     <section className='settings-page'>
       <div className='container settings-page__container'>
         <h3 className='dark-text dark-text__container'>Personal information</h3>
         <form className='settings-form container__settings-form'>
-          <div>
-            <label htmlFor='user-avatar'>
+          <div className='setter-photos settings-form__setter-photos'>
+            <div className='setter-photo__user-avatar'>
+              <label htmlFor='user-avatar'>
+                <img
+                  src={userAvatar}
+                  alt='avatar'
+                  width='72'
+                  height='72'
+                  className='setter-photos__avatar'
+                />
+                <input
+                  type='file'
+                  id='user-avatar'
+                  className='none'
+                  accept='image/,.png,.jpeg,.jpg'
+                  onChange={(e) => {
+                    if (e.target.files[0]) {
+                      const newBackgroundUrl = URL.createObjectURL(
+                        e.target.files[0]
+                      );
+                      setBackground(newBackgroundUrl);
+                    }
+                  }}
+                />
+              </label>
+              <div>
+                <p className='text setter-photos__text'>{userName}</p>
+                <p className='common-text setter-photos__common-text'>
+                  @{userNickName}
+                </p>
+              </div>
+            </div>
+
+            <label
+              htmlFor='user-background'
+              className='settings-form__user-background'
+            >
+              <p className='common-text user-background__common-text'>
+                Background image
+              </p>
               <img
-                src={`./photos/usersAvatar/${currentUsersProfile.profileAvatar}`}
+                src={userBackground}
                 alt='avatar'
-                width='72'
-                height='72'
+                width='325'
+                height='105'
+                className='setter-photos__backround'
+              />
+              <input
+                type='file'
+                id='user-background'
+                className='none'
+                accept='image/,.png,.jpeg,.jpg'
+                onChange={(e) => {
+                  if (e.target.files[0]) {
+                    const newAvatarUrl = URL.createObjectURL(e.target.files[0]);
+                    setUserAvatar(newAvatarUrl);
+                  }
+                }}
               />
             </label>
-            <input
-              type='file'
-              id='user-avatar'
-              className='none'
-              accept='image/,.png,.jpeg,.jpg'
-              onChange={(e) => {
-                setUserAvatar();
-              }}
+            <SettingBlock
+              inputValue={userName}
+              setInputValue={setUserName}
+              label={'Name'}
+              inputId={'user-name'}
+              inputType={'text'}
+              size={'standard-input'}
+            />
+            <SettingBlock
+              inputValue={userNickName}
+              setInputValue={setUserNickName}
+              label={'Nickname'}
+              inputId={'user-nickname'}
+              inputType={'text'}
+              size={'standard-input'}
+            />
+            <SettingBlock
+              inputValue={userProfileDescription}
+              setInputValue={setUserProfileDescription}
+              label={'Description'}
+              inputId={'user-description'}
+              inputType={'text'}
+              size={'textholder-input'}
             />
           </div>
         </form>
