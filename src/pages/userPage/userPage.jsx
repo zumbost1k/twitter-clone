c21f92625d76d3006a44e93edcea59dc7538e539
zumbost1k <matusevich.mi@gmail.com>
2023-12-20 21:31:10 +0300

    add retweets posts to the page

--- src/pages/userPage/userPage.jsx
+++ src/pages/userPage/userPage.jsx
@@ -1,41 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './userPage.css';
 import ProfileHeader from '@/components/profileHeader/profileHeader';
 import ContentFilter from '@/components/contentFilter/contentFilter';
 import AllNews from '@/components/allNews/allNews';
-import { useSelector } from 'react-redux';
-import { selectallNews } from '@/selectors/selectors';
 
 const UserPage = () => {
-  const userPageNews = useSelector(selectallNews);
-  return (
-    <section>
-      <ProfileHeader />
-      <div className='filtered-news'>
-        <ContentFilter
-          filterLinks={[
-            {
-              labelText: 'Tweets',
-              id: 'tweets',
-            },
-            {
-              labelText: 'Tweets & replies',
-              id: 'replies',
-            },
-            {
-              labelText: 'Media',
-              id: 'media',
-            },
-            {
-              labelText: 'Likes',
-              id: 'likes',
-            },
-          ]}
-        />
-        <AllNews isUserPage={true} allNews={userPageNews}/>
-      </div>
-    </section>
-  );
+  const [userPageNews, setUserPageNews] = useState(null);
+  useEffect(() => {
+    fetch(
+      `https://twittercloneapiproductionenv.azurewebsites.net/Tweet/GetCurrentUserTweetsAndRetweets`,
+      {
+        method: 'GET',
+        credentials: 'include',
+        withCredentials: true,
+        crossorigin: true,
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setUserPageNews(data.data);
+      });
+  }, []);
+
+  if (userPageNews) {
+    return (
+      <section>
+        <ProfileHeader />
+        <div className='filtered-news'>
+          <ContentFilter
+            filterLinks={[
+              {
+                labelText: 'Tweets',
+                id: 'tweets',
+              },
+              {
+                labelText: 'Tweets & replies',
+                id: 'replies',
+              },
+              {
+                labelText: 'Media',
+                id: 'media',
+              },
+              {
+                labelText: 'Likes',
+                id: 'likes',
+              },
+            ]}
+          />
+          <AllNews isUserPage={true} allNews={userPageNews} />
+        </div>
+      </section>
+    );
+  }
 };
 
 export default UserPage;
